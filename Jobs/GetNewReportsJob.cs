9915aeb8ed6a38d415caeb1667c86d504bde04c6
warrinerbros <nicholas.warriner@protonmail.com>
2022-07-30 17:07:15 -0400

    Move LoadReports to GetNewReportsJob.cs

--- Jobs/GetNewReportsJob.cs
+++ Jobs/GetNewReportsJob.cs
@@ -1,15 +1,33 @@
+using System.Diagnostics.CodeAnalysis;
 using ICSharpCode.SharpZipLib.GZip;
 using ICSharpCode.SharpZipLib.Tar;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using ProtonDbApi.Models;
+using ProtonDbApi.Repos;
 
 namespace ProtonDbApi.Jobs;
 
-public static class GetNewReportsJob
+public interface IGetNewReportsJob
 {
-    public static async Task GetAndExtractReportsFromGithub()
+    Task GetAndExtractReportsFromGithub();
+}
+
+public class GetNewReportsJob : IGetNewReportsJob
+{
+    private ILogger<GetNewReportsJob> Logger;
+    private ReportContext Context;
+
+    public GetNewReportsJob(ILogger<GetNewReportsJob> logger, ReportContext context)
+    {
+        Logger = logger;
+        Context = context;
+    }
+
+    public async Task GetAndExtractReportsFromGithub()
     {
         var now = DateTime.Now;
         var month = now.ToString("MMM").ToLower();
-        // var day = now.Day;
         var day = 1;
         var year = now.Year;
         var tarPath = @"Reports/download.tar.gz";
@@ -17,14 +35,14 @@ public static class GetNewReportsJob
         var tarFileName = month + day + "_" + year + ".tar.gz";
         var reportUrl = "https://github.com/bdefore/protondb-data/raw/master/reports/reports_" + tarFileName;
 
+        // Remove any old report files
         if (File.Exists(tarPath))
         {
             File.Delete(tarPath);
         }
-
-        if (File.Exists(tarDir + "reports_piiremoved.json"))
+        if (File.Exists(tarDir + "/reports_piiremoved.json"))
         {
-            File.Delete(tarPath);
+            File.Delete(tarDir + "/reports_piiremoved.json");
         }
 
         var httpClient = new HttpClient();
@@ -40,9 +58,9 @@ public static class GetNewReportsJob
         }
         catch (Exception ex)
         {
-            var errorString = "Error fetching tar.gz file from github";
+            Logger.LogError(ex,"Error fetching tar.gz file from github");
         }
-        
+
         try
         {
             var inStream = File.OpenRead(tarPath);
@@ -50,11 +68,77 @@ public static class GetNewReportsJob
             var tarArchive = TarArchive.CreateInputTarArchive(gzipStream);
             tarArchive.ExtractContents(tarDir);
             tarArchive.Close();
+            gzipStream.Close();
             inStream.Close();
         }
         catch (Exception ex)
         {
-            var errorString = "Failed to extract downloaded tar.gz file";
+            Logger.LogError(ex,"Failed to extract downloaded tar.gz file");
+        }
+
+        await LoadReports();
+    }
+    
+    private async Task LoadReports()
+    {
+        const string path = "Reports/reports_piiremoved.json";
+        var json = await File.ReadAllTextAsync(path);
+        
+        // Remove non-ascii characters
+        var specialCharactersRemovedJson = string.Concat(json.Where(c => c < 255));
+        
+        var reportList = JsonConvert.DeserializeObject<List<RawReport>>(specialCharactersRemovedJson);
+        if (reportList != null)
+        {
+            var parsedReports = reportList.Select(rawReport => new Reports
+            {
+                AppId = Int32.Parse(rawReport.App.Steam.AppId),
+                Title = rawReport.App.Title,
+                AppSelectionMethod = rawReport.Responses.AppSelectionMethod,
+                AudioFaults = rawReport.Responses.AudioFaults,
+                Duration = rawReport.Responses.Duration,
+                Extra = rawReport.Responses.Extra,
+                GraphicalFaults = rawReport.Responses.GraphicalFaults,
+                InputFaults = rawReport.Responses.InputFaults,
+                Installs = rawReport.Responses.Installs,
+                IsImpactedByAntiCheat = rawReport.Responses.IsImpactedByAntiCheat,
+                IsMultiplayerImportant = rawReport.Responses.IsMultiplayerImportant,
+                LocalMultiplayerAttempted = rawReport.Responses.LocalMultiplayerAttempted,
+                Notes = rawReport.Responses.Notes,
+                OnlineMultiplayerAttempted = rawReport.Responses.OnlineMultiplayerAttempted,
+                Opens = rawReport.Responses.Opens,
+                PerformanceFaults = rawReport.Responses.PerformanceFaults,
+                ProtonVersion = rawReport.Responses.ProtonVersion,
+                SaveGameFaults = rawReport.Responses.SaveGameFaults,
+                SignificantBugs = rawReport.Responses.SignificantBugs,
+                StabilityFaults = rawReport.Responses.StabilityFaults,
+                StartsPlay = rawReport.Responses.StartsPlay,
+                Type = rawReport.Responses.Type,
+                Verdict = rawReport.Responses.Verdict,
+                WindowingFaults = rawReport.Responses.WindowingFaults,
+                Timestamp = rawReport.Timestamp,
+                Cpu = rawReport.SystemInfo.Cpu,
+                Gpu = rawReport.SystemInfo.Gpu,
+                GpuDriver = rawReport.SystemInfo.GpuDriver,
+                Kernel = rawReport.SystemInfo.Kernel,
+                Os = rawReport.SystemInfo.Os,
+                Ram = rawReport.SystemInfo.Ram
+            });
+
+            try
+            {
+                // Remove any existing rows
+                await Context.Database.ExecuteSqlRawAsync("DELETE FROM Notes");
+                await Context.Database.ExecuteSqlRawAsync("DELETE FROM Reports");
+
+                // Add new rows
+                Context.Reports.AddRange(parsedReports);
+                await Context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, "Could not add report insert parsed reports");
+            }
         }
     }
 }
